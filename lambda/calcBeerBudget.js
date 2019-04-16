52b3954f76890d342a6d51e70d2c5cfe265c8828
Andy Barnes <andressbarnes@gmail.com>
2019-04-16 14:29:18 -0700

    initial commit

---
+++ lambda/calcBeerBudget.js
@@ -0,0 +1,12 @@
+exports.handler = async event => {
+  let promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      const beerBudget = event.age * 20;
+
+      res({ name: event.name, beerBudget: beerBudget });
+    }, 100);
+  });
+
+  const response = await promise;
+  return response;
+};
